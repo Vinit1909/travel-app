de70c44332086a53f73ea62f05541579ff7c3fa0
Vinit Jahagirdar <115438154+Vinit1909@users.noreply.github.com>
2023-12-25 17:13:03 -0800

    fixed import issue

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import Image from "next/Image";
+import Image from "next/image";
 
 type ButtonProps = {
     type: 'button' | 'submit';
@@ -7,11 +7,11 @@ type ButtonProps = {
     variant: 'btn_dark_green'
 }
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
     return (
-        <button 
-        type={type}
-        className={`flexCenter gap-3 rounded-full border ${variant}`}
+        <button
+            type={type}
+            className={`flexCenter gap-3 rounded-full border ${variant}`}
         >
             {icon && <Image src={icon} alt={title} width={24} height={24} />}
             <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
